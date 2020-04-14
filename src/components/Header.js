6901aaaa26ef85ff759dababc63b06155fd2964e
Łukasz Wiącek <lukasz.wiacek@mayeryn.com>
2020-04-14 17:43:22 +0200

    improve RWD styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,15 @@ import { PokemonFilterContext } from '../context/PokemonFilterContext';
 import FilterPokemons from './FilterPokemons';
 
 const Header = () => {
-  const { menu, menuToggle } = useContext(PokemonFilterContext);
+  const { menu, menuToggle, nameFilter } = useContext(PokemonFilterContext);
 
   return (
     <div className="header">
       <h1>Pokedex</h1>
       <button className="menuBtn" onClick={() => menuToggle()}>
         {menu ? 'Close Filters' : 'Find Pokemon'}
+        {nameFilter ? '/ Show all' : ''}
       </button>
-
       {menu ? <FilterPokemons /> : <React.Fragment />}
     </div>
   );

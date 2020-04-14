4c79e0d513c3172b7ee74d61cca0573b923d1abc
Łukasz Wiącek <lukasz.wiacek@mayeryn.com>
2020-04-14 18:40:33 +0200

    Updates

---
+++ precache-manifest.05f0a5a0b8a3542952e39af0ec395aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff889c48644c312f2a1e1fd295723a7e",
+    "url": "/Poke-Dex/index.html"
+  },
+  {
+    "revision": "f7c9d529b01800e25424",
+    "url": "/Poke-Dex/static/css/main.810caeda.chunk.css"
+  },
+  {
+    "revision": "fcf54679c985b275cec7",
+    "url": "/Poke-Dex/static/js/2.f3b843a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Poke-Dex/static/js/2.f3b843a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c9d529b01800e25424",
+    "url": "/Poke-Dex/static/js/main.7bd8dac0.chunk.js"
+  },
+  {
+    "revision": "a01798e153e0662aa581",
+    "url": "/Poke-Dex/static/js/runtime-main.941d876e.js"
+  }
+]);
